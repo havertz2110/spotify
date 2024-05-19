68e14cdc5fe88fe3900a2c929463503e67c2ff29
havertz2110 <buivuongtamanh21102004@gmail.com>
2024-05-19 13:30:19 +0700

    fix login ejs
    uhhhh im tired of thisss

--- app.js
+++ app.js
@@ -102,7 +102,7 @@ app.post("/login", async function(req, res){
   try {
       // check if the user exists
       const detailsCollection = db.collection('details');
-      const user = await detailsCollection.findOne({ username:req.body.username});
+      const user = await detailsCollection.findOne({ name:req.body.name});
      
       if (user) {
      // Lấy salt từ người dùng trong cơ sở dữ liệu
@@ -111,13 +111,14 @@ app.post("/login", async function(req, res){
      // Băm mật khẩu nhập vào với salt
      const hashedPassword = crypto.createHmac('sha256', salt).update(req.body.password).digest('hex');
       const result = hashedPassword === user.password;
-     if (!result) {
+     if (result) {
        res.redirect('/main');
         } else {
       
           res.status(400).json({ error: "password doesn't match" });
         }
-      } else {
+      } 
+      else {
         res.status(400).json({ error: "User doesn't exist" });
       }
     } catch (error) {
@@ -128,7 +129,6 @@ app.post("/login", async function(req, res){
 app.get("/main", function (req, res) {
   res.render("main");
 });
-
 
 
 
